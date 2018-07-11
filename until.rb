f7a96e36ff4473a87dd3478a4fc9ff5897d40570
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-11 14:21:17 +0000

    Done.

---
+++ until.rb
@@ -0,0 +1,8 @@
+def hungry_gp
+  mango= 0
+  until mango == 3
+  puts "Give me my mango"
+  mango += 1
+end 
+end 
+hungry_gp
---
+++ while.rb
@@ -0,0 +1,10 @@
+
+def hungry_mn
+  kitkat= 0
+  while kitkat < 30
+  puts "Give me more Kitkats"
+  kitkat += 1
+end 
+end 
+hungry_mn
+  
